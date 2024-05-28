e4dd07df4f2398b58b8423bd98f69ce5cab418d9
krrod7 <krrod7@bu.edu>
2024-05-27 23:13:17 -0400

    Initial commit for Node.js backend

---
+++ config/cocktailConfig.js
@@ -0,0 +1,7 @@
+
+const cocktailConfig = {
+    url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+    //queryString: `?s=${searchTerm}` //.... had to put in other file to pass search term from angular
+}
+
+//module.exports = cocktailConfig;
---
+++ routes/drinks.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const cocktailConfig = require('../config/cocktailConfig');
+const Redis = require('ioredis');
+const redis = new Redis();
+
+//matches http://localhost:3000/drinks
+//
+router.get('/', async (req, res, next) => {
+    const searchTerm = req.query.search
+    const cocktailConfig = {
+        url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+        queryString: `?s=${searchTerm}`
+    }
+
+    try {
+        let cachedData = await redis.get("drinkInfo");
+        if (cachedData) {
+            let parsedData = await JSON.parse(cachedData);
+            console.log(`Cached: ${parsedData}`);
+            parsedData.cached = true;
+            res.json(parsedData)
+        }
+        let rawDrinkInfo = await fetch(cocktailConfig.url + cocktailConfig.queryString)
+        let parsedData = await rawDrinkInfo.json();
+
+
+
+        console.table(`drinks: ${parsedData}`);
+        let response = await redis.set("drinkInfo", parsedData, "EX", 15);
+        parsedData.cached = false;
+        res.json(parsedData);
+    } catch (err) {
+        console.log(`Error: ${err}`);
+    }
+})
+module.exports = router; //the export makes router visible to any file that require()s it
