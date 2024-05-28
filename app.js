0ae538c6ec54d601c53bd45f4b0895ab56ef0d3c
krrod7 <krrod7@bu.edu>
2024-05-28 01:04:49 -0400

    cors 4

--- app.js
+++ app.js
@@ -11,7 +11,20 @@ var drinksRouter = require('./routes/drinks');
 
 var app = express();
 
-app.use(cors())
+// Set up CORS
+app.use(cors({
+  origin: true, // "true" will copy the domain of the request back
+                // to the reply. If you need more control than this
+                // use a function.
+
+  credentials: true, // This MUST be "true" if your endpoint is
+                     // authenticated via either a session cookie
+                     // or Authorization header. Otherwise the
+                     // browser will block the response.
+
+  methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
+                                         // pre-flight OPTIONS requests
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
